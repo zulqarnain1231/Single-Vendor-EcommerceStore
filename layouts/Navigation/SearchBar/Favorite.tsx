5c5217dbdf64eb1c6b3ec1b27db767fd3d91ce9f
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-19 14:09:45 -0700

    fixed the search bar section

--- layouts/Navigation/SearchBar/Favorite.tsx
+++ layouts/Navigation/SearchBar/Favorite.tsx
@@ -5,11 +5,8 @@ import { MdFavoriteBorder } from "react-icons/md";
 function Favorite() {
   return (
     <div>
-      <Badge
-        badgeContent={4}
-    
-      >
-        <MdFavoriteBorder className="text-[30px] cursor-pointer" />
+      <Badge badgeContent={4}>
+        <MdFavoriteBorder className="text-[26px] sm:text-[30px] cursor-pointer" />
       </Badge>
     </div>
   );
