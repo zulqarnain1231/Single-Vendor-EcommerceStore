e0a9e25bf577484c3a4293619e7f9be27f51fb6a
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-19 11:25:54 -0700

    added dummy data for home page in datajs file

---
+++ constants/Data/JSON.js
@@ -0,0 +1,92 @@
+export const Data = {
+  SearchHeader: {
+    SearchCategory: [
+      {
+        Title: "Electronics",
+        Value: "Electronics",
+      },
+      {
+        Title: "Game",
+        Value: "Game",
+      },
+      {
+        Title: "Mobile",
+        Value: "Mobile",
+      },
+      {
+        Title: "Lifestyle",
+        Value: "Lifestyle",
+      },
+      {
+        Title: "Men's Fashion",
+        Value: "Men's Fashio",
+      },
+      {
+        Title: "Television",
+        Value: "Television",
+      },
+    ],
+  },
+  ProductsSliderData: [
+    {
+      title: "SLIM FIT PEPPERMINT COTTON TEE",
+      price: 33.0,
+      new: true,
+      soldOut: false,
+      picture: "/Assets/home/Shirt.jpg",
+    },
+    {
+      title: "SLIM FIT Black COTTON TEE",
+      price: 28.0,
+      new: false,
+      sale: "20% Off",
+      picture: "/Assets/home/shirt2.jpg",
+      soldOut: false,
+    },
+    {
+      title: "Apple Watch Pro",
+      price: 450.0,
+      new: false,
+      sale: "10% Off",
+      soldOut: true,
+      picture: "/Assets/home/smartwatch.jpeg",
+    },
+    {
+      title: "Nike Addidas Shoes",
+      price: 230.0,
+      new: false,
+      soldOut: false,
+      picture: "/Assets/home/shoes.webp",
+    },
+    {
+      title: "SLIM FIT PEPPERMINT COTTON TEE",
+      price: 33.0,
+      new: true,
+      soldOut: false,
+      picture: "/Assets/home/Shirt.jpg",
+    },
+    {
+      title: "SLIM FIT Black COTTON TEE",
+      price: 28.0,
+      new: false,
+      sale: "20% Off",
+      picture: "/Assets/home/shirt2.jpg",
+      soldOut: false,
+    },
+    {
+      title: "Apple Watch Pro",
+      price: 450.0,
+      new: false,
+      sale: "10% Off",
+      soldOut: true,
+      picture: "/Assets/home/smartwatch.jpeg",
+    },
+    {
+      title: "Nike Addidas Shoes",
+      price: 230.0,
+      new: false,
+      soldOut: false,
+      picture: "/Assets/home/shoes.webp",
+    },
+  ],
+};
