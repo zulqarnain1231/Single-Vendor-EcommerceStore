c5892e986145565b29122be67ae268210ad2363b
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-19 14:01:21 -0700

    created custom typography components and updated dummydata

--- constants/Data/JSON.js
+++ constants/Data/JSON.js
@@ -1,32 +1,175 @@
+
+import { AiFillInstagram } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
+
+
 export const Data = {
-  SearchHeader: {
+  topSection: {
     SearchCategory: [
       {
-        Title: "Electronics",
-        Value: "Electronics",
+        name: "t-thirts",
+        path: "",
       },
       {
-        Title: "Game",
-        Value: "Game",
+        name: "button-down shirts",
+        path: "",
       },
       {
-        Title: "Mobile",
-        Value: "Mobile",
+        name: "Polo Shirts",
+        path: "",
       },
       {
-        Title: "Lifestyle",
-        Value: "Lifestyle",
+        name: "Button-Down Shirts",
+        path: "",
       },
       {
-        Title: "Men's Fashion",
-        Value: "Men's Fashio",
+        name: "Long-Sleeve Shirts",
+        path: "",
       },
       {
-        Title: "Television",
-        Value: "Television",
+        name: "Short-Sleeve Shirts",
+        path: "",
+      },
+      {
+        name: "Henley Shirts",
+        path: "",
+      },
+    ],
+
+    AllCategories: [
+      {
+        name: "t-thirts",
+        path: "",
+        // Icon: <MdElectricalServices size={16} color="#1D1D1D" />,
+      },
+      {
+        name: "button-down shirts",
+        path: "",
+      },
+      {
+        name: "Polo Shirts",
+        path: "",
+      },
+      {
+        name: "Button-Down Shirts",
+        path: "",
+      },
+      {
+        name: "Long-Sleeve Shirts",
+        path: "",
+      },
+      {
+        name: "Short-Sleeve Shirts",
+        path: "",
+      },
+      {
+        name: "Henley Shirts",
+        path: "",
+      },
+      {
+        name: "Graphic Shirts",
+        path: "",
+      },
+      {
+        name: "Oxford Shirts",
+        path: "",
+      },
+      {
+        name: "Flannel Shirts",
+        path: "",
+      },
+      {
+        name: "Sports Shirts",
+        path: "",
+      },
+      {
+        name: "Work Shirts",
+        path: "",
+      },
+    ],
+    Menu: [
+      {
+        name: "home",
+      },
+      {
+        name: "about us",
+      },
+      {
+        name: "privacy policy",
+      },
+      {
+        name: "faq",
       },
     ],
   },
+
+  footer : [
+    {
+      title: "about us",
+      links: [
+        {
+          name: "About us",
+          path: "#",
+        },
+        {
+          name: "contact us",
+          path: "#",
+        },
+      ],
+    },
+    {
+      title: "privacy",
+      links: [
+        {
+          name: "Shipping & Return",
+          path: "#",
+        },
+        {
+          name: "Privacy Policy",
+          path: "#",
+        },
+        {
+          name: "Terms & Conditions",
+          path: "#",
+        },
+      ],
+    },
+    {
+      title: "find us",
+      links: [
+        {
+          name: "support@outlander.com.pk",
+        },
+        {
+          name: "+92 332-4975571",
+        },
+        {
+          name: "Mon-Fri: 10:00 A.M – 6:00 P.M",
+        },
+      ],
+    },
+    {
+      title: "follow us",
+      links: [
+        {
+          name: (
+            <AiFillInstagram className="text-white-main text-[36px] hover:scale-125 duration-300" />
+          ),
+        },
+        {
+          name: (
+            <BsFacebook className="text-white-main text-[36px] hover:scale-125 duration-300" />
+          ),
+        },
+        {
+          name: (
+            <FaTwitter className="text-white-main text-[36px] hover:scale-125 duration-300" />
+          ),
+        },
+      ],
+    },
+  ],
   ProductsSliderData: [
     {
       title: "SLIM FIT PEPPERMINT COTTON TEE",
