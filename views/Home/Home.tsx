3355ee9d52a979826f9dee91f5921dcb60d3d04d
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-05-20 02:14:30 -0700

    fixed the  best products slider

--- views/Home/Home.tsx
+++ views/Home/Home.tsx
@@ -1,57 +1,13 @@
 import React from "react";
 import Wrapper from "../../components/shared/ComponentWrapper/Wrapper";
-import ProductCard from "../../components/shared/ProductCards/ProductCard";
-import { Data } from "../../constants/Data/JSON";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination, Mousewheel, Autoplay } from "swiper";
 import Hero from "./Hero";
+import OurBestSellingProducts from "./OurBestSellingProducts";
 function Home() {
   return (
     <Wrapper>
-      <Hero />
-      <div className="h-[300px] max-w-[375px] sm:max-w-screen-sm md:max-w-screen-md  lg:max-w-full my-10">
-        <Swiper
-          slidesPerView={4}
-          spaceBetween={20}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            300: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 4,
-              spaceBetween: 50,
-            },
-          }}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          modules={[Pagination, Mousewheel, Autoplay]}
-          className="mySwiper"
-        >
-          {Data.ProductsSliderData.map((item: any, index: number) => (
-            <SwiperSlide key={index}>
-              <ProductCard
-                title={item.title}
-                price={item.price}
-                newArrival={item.new}
-                sale={item.sale}
-                soldout={item.soldOut}
-                picture={item.picture}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      <div className="w-full flex flex-col gap-10">
+        <Hero />
+        <OurBestSellingProducts />
       </div>
     </Wrapper>
   );
