05aaea8e94f931a85477913fba9d7e11b856b44d
Muhammad zulqarnain <zulqarnainniazi447@gmail.com>
2023-06-18 00:19:50 -0700

    added dashboard route for admin

---
+++ views/Admin/Dashboard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import StatsSnippet from "../../src/components/Admin/Snippets/Stats";
+import SalesSnippet from "../../src/components/Admin/Snippets/Sales";
+import { Data } from "../../constants/Data/JSON";
+import RecentOrders from "@/components/Admin/Snippets/RecentOrders";
+import RecentBuyer from "@/components/Admin/Snippets/RecentBuyer";
+
+const Dashboard = () => {
+
+  const { snippetsData } = Data.AdminData;
+
+  return (
+    <div className="w-full flex flex-col gap-6 sm:p-4 p-2">
+      <div className="w-full grid lg:grid-cols-3 gap-4">
+        <StatsSnippet
+          dataKey="orders"
+          Data={snippetsData}
+          Title="Orders"
+          Figure={40}
+        />
+        <StatsSnippet
+          dataKey="orders"
+          Data={snippetsData}
+          Title="Sales"
+          Figure={1400}
+        />
+        <StatsSnippet
+          dataKey="orders"
+          Data={snippetsData}
+          Title="New Buyers"
+          Figure={130}
+        />
+      </div>
+      <SalesSnippet />
+      <RecentOrders />
+      {/* ===> recent buyer here */}
+      <RecentBuyer/>
+    </div>
+  );
+};
+
+export default Dashboard;
